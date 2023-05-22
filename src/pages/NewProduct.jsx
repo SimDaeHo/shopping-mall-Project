2974a9dd209ae0270c2bd124ba827ba6e0c8d43c
daeho sim <tlaeogh5@gmail.com>
2023-05-22 16:35:30 +0900

    업로드 보여주기

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -5,6 +5,8 @@ import { addNewProduct } from "../api/firebase";
 export default function NewProduct() {
   const [product, setProduct] = useState({});
   const [file, setFile] = useState();
+  const [isUploading, setIsUploading] = useState(false);
+  const [success, setSuccess] = useState();
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
@@ -16,13 +18,22 @@ export default function NewProduct() {
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    uploadImage(file).then((url) => {
-      console.log(url);
-      addNewProduct(product, url);
-    });
+    setIsUploading(true);
+    uploadImage(file)
+      .then((url) => {
+        addNewProduct(product, url).then(() => {
+          setSuccess("성공적으로 제품이 추가되었습니다.");
+          setTimeout(() => {
+            setSuccess(null);
+          }, 4000);
+        });
+      })
+      .finally(() => setIsUploading(false));
   };
   return (
     <section>
+      <h2>새로운 제품 등록</h2>
+      {success && <p>✅{success}</p>}
       {file && <img src={URL.createObjectURL(file)} alt="local file" />}
       <form onSubmit={handleSubmit}>
         <input type="file" accept="image/*" name="file" required onChange={handleChange} />
@@ -31,7 +42,7 @@ export default function NewProduct() {
         <input type="text" name="category" value={product.category ?? ""} placeholder="카테고리" required onChange={handleChange} />
         <input type="text" name="description" value={product.description ?? ""} placeholder="제품 설명" required onChange={handleChange} />
         <input type="text" name="options" value={product.options ?? ""} placeholder="옵션들(콤마(,)로 구분)" required onChange={handleChange} />
-        <Button text={"제품 등록하기"} />
+        <Button text={isUploading ? "업로드중..." : "제품 등록하기"} disabled={isUploading} />
       </form>
     </section>
   );
